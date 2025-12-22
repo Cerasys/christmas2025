112f2988b5bb03582776c249baad2a0f267b669f
Nathan <nathanblee@gmail.com>
2025-12-22 15:34:36 -0800

    exported constants

---
+++ src/components/Stages.js
@@ -0,0 +1,78 @@
+export const FINAL_VIDEO_ID = "FINAL_VIDEO_ID";
+
+export const STAGES = [
+  {
+    id: 1,
+    code: "friends",
+    videoId: "hiADHw5k62E",
+    hint: {
+      type: "video",
+      videoId: "ZpG7FIsAy9o",
+    },
+  },
+  {
+    id: 2,
+    code: "stomach",
+    videoId: "JZZRbn8erAk",
+    hint: {
+      type: "text",
+      content: "Your Animal Friends are Stuffed Within a Box...",
+    },
+  },
+  {
+    id: 3,
+    code: "professionalchef",
+    videoId: "jzB21s2-Zt4",
+    hint: {
+      type: "text",
+      content:
+        "Where do you keep this main ingredient of this dish? https://www.allrecipes.com/recipe/13107/miso-soup/",
+    },
+  },
+  {
+    id: 4,
+    code: "bulldogs",
+    videoId: "Ja-Yb9ChicA",
+    hint: {
+      type: "text",
+      content: "Go to your stovetop and look up",
+    },
+  },
+  {
+    id: 5,
+    code: "genki",
+    videoId: "VIDEO_ID_5",
+    hint: {
+      type: "text",
+      content: "It might be behind this 'Bulldogs' themed emotion chart?",
+    },
+  },
+  {
+    id: 6,
+    code: "girl",
+    videoId: "VIDEO_ID_6",
+    hint: {
+      type: "text",
+      content:
+        "https://www.health.harvard.edu/blog/why-teenagers-eat-tide-pods-2018013013241",
+    },
+  },
+  {
+    id: 7,
+    code: "engaged",
+    videoId: "VIDEO_ID_7",
+    hint: {
+      type: "text",
+      content: "The code is 'ENGAGED'",
+    },
+  },
+  {
+    id: 8,
+    code: "done",
+    videoId: "VIDEO_ID_8",
+    hint: {
+      type: "text",
+      content: "How would you swap a photo out of its frame?",
+    },
+  },
+];
